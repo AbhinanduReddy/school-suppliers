f373e2c84af37ecea46a3d47a8e1c8d858608ef0
AbhinanduReddy <nanduc06@gmail.com>
2023-11-01 15:54:01 -0700

    fix(cookies): last five visited products

--- product.php
+++ product.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -15,7 +16,7 @@
             <ul>
                 <li><a href="index.php">Home</a></li>
                 <li><a href="about.php">About</a></li>
-                <li><a href="product.php">Products</a></li>
+                <li><a href="products.php">Products</a></li>
                 <li><a href="news.php">News</a></li>
 
                 <li><a href="contact.php">Contact</a></li>
@@ -27,18 +28,63 @@
     <div class="container" style="background-image: url('ABOUT.jpg'); color:white; background-repeat: no-repeat;background-size: cover;">
         <!-- <img class="image" src="home_im.jpeg" alt="Image"> -->
         <div class="center-container" >
-        <h2>Our Product Range</h2>
-        <p>Explore our extensive product range, including:</p>
-        <ul>
-            <li>Stationery</li>
-            <li>Art Supplies</li>
-            <li>Educational Materials</li>
-            <li>Backpacks and Bags</li>
-            <li>Classroom Essentials</li>
-            <li>Technology Accessories</li>
-            <li>Office Supplies</li>
-            <li>And Much More</li>
-        </ul>
+        <?php
+        
+    if (isset($_GET['name']) && isset($_GET['img']) && isset($_GET['price']) && isset($_GET['des'])) {
+        $productName = $_GET['name'];
+        $productDescription = $_GET['des'];
+        $productPrice = $_GET['price'];
+        $img = $_GET['img'];
+        $pn =  array($productName,$productDescription, $productPrice, $img);
+        
+        $productNameJ=implode('~',$pn);
+        $existingCookie = isset($_COOKIE['visitedProducts']) ? $_COOKIE['visitedProducts'] : '';
+        $productCountCookie = isset($_COOKIE[$productName]) ? $_COOKIE[$productName] : '';
+
+
+        $productsArray = explode('`', $existingCookie);
+        $found=false;
+        foreach ($productsArray as $key=>$productt) {
+            $product= explode('~',$productt);
+            if(isset($product[0]) && isset($product[1]) && isset($product[2]) && isset($product[3]) ){
+                if($product[0]== $productName){
+                    unset($productsArray[$key]);
+                }
+            }
+        }
+        $productsArray = array_values($productsArray);
+
+        array_unshift($productsArray, $productNameJ);
+        if (count($productsArray) > 5) {
+            array_pop($productsArray);
+        }
+      
+        if($productCountCookie==''){
+            $productCountCookie=1;
+        }
+        else{
+            $productCountCookie = (int)$productCountCookie+1;
+        }
+
+        // Set the cookie with the updated products array
+        setcookie('visitedProducts', implode('`', $productsArray), strtotime('Fri, 31 Dec 9999 23:59:59 GMT'), '/');
+        setcookie($productName,$productCountCookie , strtotime('Fri, 31 Dec 9999 23:59:59 GMT'), '/');
+
+
+
+        echo '<div class="product" style="">';
+        echo '<img src="' . $img . '" alt="Product Image" width="150" height="150">';
+        echo '<h2>' . $productName . '</h2>'; 
+        echo '<p>Description: ' . urldecode($productDescription) . '</p>';
+        echo '<p>Price: $' . $productPrice . '</p>';
+      
+        echo '</div>';
+    } else {
+        echo '<p>No product information found.</p>';
+    }
+    ?>
+       
+</div>
 </div>
         
     </div>
--- products.php
+++ products.php
@@ -19,7 +19,21 @@ if (isset($_SESSION['username'])) {
 
     <title>School Supplies</title>
     <style>
-        
+        .button-link {
+    display: inline-block;
+    padding: 10px 20px;
+    background-color: #3498db;
+    color: #fff;
+    text-decoration: none;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+/* Style for the button on hover */
+.button-link:hover {
+    background-color: #2580b3;
+}
     </style>
 </head>
 <body>
@@ -48,21 +62,94 @@ if (isset($_SESSION['username'])) {
        
     </header>
  
-    <div class="container" style="background-image: url('ABOUT.jpg'); color:white; background-repeat: no-repeat;background-size: cover;">
+    <div  >
         <!-- <img class="image" src="home_im.jpeg" alt="Image"> -->
-        <div class="center-container" >
-        <h2>Our Product Range</h2>
+        <div  >
+        <h2>Our Products</h2>
+        <a class="button-link" href='/school-suppliers/visitedProducts.php'>Last Visited Products</a>
         <p>Explore our extensive product range, including:</p>
-        <ul>
-            <li>Stationery</li>
-            <li>Art Supplies</li>
-            <li>Educational Materials</li>
-            <li>Backpacks and Bags</li>
-            <li>Classroom Essentials</li>
-            <li>Technology Accessories</li>
-            <li>Office Supplies</li>
-            <li>And Much More</li>
-        </ul>
+        <?php
+// Mocked product data
+$products = array(
+    array(
+        'product_name' => 'Color Pencils',
+        'img'=>'https://i0.wp.com/johnmuirlaws.com/wp-content/uploads/2020/09/pencils-store.jpg?fit=1000%2C1333&ssl=1',
+        'product_description' => 'Polychromos Artists’ Color Pencils, Tin of 24 Plus 2 more!',
+
+        'product_price' => 19.99,
+    ),
+    array(
+        'product_name' => 'Books',
+        'img'=>'https://nestambuy.com/wp-content/uploads/2021/07/small-unruled_2-500x500.jpg',
+
+        'product_description' => 'ITC Classmate Long Note Book (UR) 156Page',
+        'product_price' => 29.99,
+    ),
+    array(
+        'product_name' => 'Sticky Notes',
+        'img'=>'https://i.ebayimg.com/images/g/044AAOSwSlRa3lFZ/s-l1200.webp',
+        'product_description' => 'Post-it Super Sticky Notes, 3 in x 3 in, Colors 90 Sheets/Pad',
+        'product_price' => 9.99,
+    ),
+    array(
+        'product_name' => 'BackPack',
+        'img'=>'https://i.ebayimg.com/images/g/acYAAOSwrV9lIQ2x/s-l1600.jpg',
+        'product_description' => 'Backpack, Laptop Bag Travel School Shoulder Rucksack',
+        'product_price' => 9.99,
+    ),
+    array(
+        'product_name' => 'Puzzles',
+        'img'=>'https://i.ebayimg.com/images/g/EeEAAOSwaCRku7SW/s-l500.jpg',
+        'product_description' => 'School Puzzles Highlights Hidden Pictures',
+
+        'product_price' => 5.99,
+    ),
+    array(
+        'product_name' => 'Scissors',
+        'img'=>'https://i.ebayimg.com/images/g/aBMAAOSwscNj4Kug/s-l1600.jpg',
+
+        'product_description' => '4 Colors School Student Scissors Crafts Art DIY Office',
+        'product_price' => 29.99,
+    ),
+    array(
+        'product_name' => 'Lunch Box',
+        'img'=>'https://i.ebayimg.com/images/g/skcAAOSwNhJhJ7RG/s-l500.jpg',
+        'product_description' => 'Insulated Lunch Bag for School Men Women Kids',
+        'product_price' => 9.99,
+    ),
+    array(
+        'product_name' => 'Pencil Case',
+        'img'=>'https://i.ebayimg.com/images/g/EkcAAOSwslFlJuy6/s-l1600.jpg',
+        'product_description' => 'School Student Pencil Case, Coin Pouch Stationery',
+        'product_price' => 3.99,
+    ),
+    array(
+        'product_name' => 'Erasers',
+        'img'=>'https://i.ebayimg.com/images/g/kIAAAOSw1nNlHZgJ/s-l500.png',
+        'product_description' => '40 Pack Erasers for Kids Bulk - Variety of Cute Fruit',
+        'product_price' => 11.99,
+    ),
+    array(
+        'product_name' => 'Pens',
+        'img'=>'https://i.ebayimg.com/images/g/qTcAAOSw6rVe2Qdr/s-l500.jpg',
+        'product_description' => '6 ROLLER BALL PENS 2EACH RED BLUE BLACK SMOOTH WRITING',
+        'product_price' => 3.99,
+    )
+);
+
+// Loop through the mocked product data and generate product cards
+foreach ($products as $product) {
+    echo '<a style="color:white" href="/school-suppliers/product.php?name='. $product['product_name'] .'&img='. $product['img'] .'&des='. $product['product_description'] .'&price='. $product['product_price'] .'">';
+    echo '<div class="product-card" >';
+    echo '<img src="' . $product['img'] . '" alt="Product Image" width="150" height="150">';
+    echo '<h2 class="product-title">' . $product['product_name'] . '</h2>';
+    echo '<p class="product-description">' . $product['product_description'] . '</p>';
+    echo '<p class="product-price">$' . $product['product_price'] . '</p>';
+    echo '</div>';
+    echo '</a>';
+}
+?>
+
 </div>
         
     </div>
